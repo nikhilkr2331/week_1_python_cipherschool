54dec8105b2e7bdf8f9d1b1ef589e2fd12b97b1c
nikhilkr2331 <119113409+nikhilkr2331@users.noreply.github.com>
2022-12-01 16:41:55 +0530

    Add files via upload

---
+++ week_1_python_cipherschools.py
@@ -0,0 +1,24 @@
+a=255
+print(id(a))
+1650840267120
+
+[4]
+b=276
+print(id(b))
+1650919841552
+
+[5]
+c=278
+print(id(c))
+1650919839056
+
+[6]
+a='aaaaa'
+print(type(a))
+<class 'str'>
+
+[7]
+print('Nikhil kumar')
+Nikhil Kumar
+
+
